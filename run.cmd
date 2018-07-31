a9514ee2f6f74c743f8e6e9133b7441be1347873
Robert Brotherus <robert@iki.fi>
2018-07-31 20:38:04 +0300

    New service account

--- run.cmd
+++ run.cmd
@@ -1,2 +1,2 @@
-set GOOGLE_APPLICATION_CREDENTIALS=C:\Google Drive\programming\keys\glory-of-empires-authentication.json
+set GOOGLE_APPLICATION_CREDENTIALS=C:\Google Drive\programming\keys\glory-of-empires-service-2018-07-31.json
 lein repl
